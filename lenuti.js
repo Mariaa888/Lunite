32f83ade75f77e794d6b3b81a222decfc51b0b83
Mariaa888 <142675401+Mariaa888@users.noreply.github.com>
2024-06-28 14:25:15 +0300

    Add files via upload

---
+++ lenuti.js
@@ -0,0 +1,12 @@
+let sunglass = document.querySelector(".cardcarogry1");
+let eyeglass = document.querySelector(".cardcarogry2");
+
+function ShowSunglass() {
+    sunglass.style.display = 'block';
+    eyeglass.style.display = 'none';
+}
+
+function ShowEyeglass() {
+    eyeglass.style.display = 'block';
+    sunglass.style.display = 'none';
+}
