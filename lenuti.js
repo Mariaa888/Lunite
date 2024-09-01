e53f37ec8ff15dcb02bee6cdcf0a5c502058159d
Mariaa888 <142675401+Mariaa888@users.noreply.github.com>
2024-09-01 15:39:38 +0300

    Update lenuti.js

--- lenuti.js
+++ lenuti.js
@@ -2,11 +2,11 @@ let sunglass = document.querySelector(".cardcarogry1");
 let eyeglass = document.querySelector(".cardcarogry2");
 
 function ShowSunglass() {
-    sunglass.style.display = 'block';
+    sunglass.style.display = 'flex';
     eyeglass.style.display = 'none';
 }
 
 function ShowEyeglass() {
-    eyeglass.style.display = 'block';
+    eyeglass.style.display = 'flex';
     sunglass.style.display = 'none';
 }
